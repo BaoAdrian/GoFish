a8c723d26906dd63c16a4e5eb446d83267b87bdf
Adrian Bao <30382340+BaoAdrian@users.noreply.github.com>
2018-11-23 21:56:02 -0700

    Testing variable declarations
    This version contains a working model of input processing. Attempt to modify data types to follow.

--- main.c
+++ main.c
@@ -17,102 +17,123 @@ const int SUIT_LENGTH = 10;
 typedef struct card_s {
     char suit[SUIT_LENGTH];
     int value;
-    struct card_s *prev; // Pointer to the previous card in LinkedList
+    // struct card_s *prev;
     struct card_s *next; // Pointer to the next card in the LinkedList
 } card;
 
+struct card_s* pull_card_data(char line[]);
+void print_list(struct card_s *card);
+void create_list(struct card_s *card); // Function used to create the deck of cards to be used as the pool
+void add_to_end(struct card_s *head, struct card_s *tail, struct card_s *card); // Add card to end of list
+
 int main(void) {
     
+    /* Variable Declarations */
+    char line[LINE_SIZE];
+    struct card_s *temp_card = NULL;
+    temp_card = (struct card_s*)malloc(sizeof(struct card_s));
+    
+    
     // Declare head and tail pointer to keep track of each end of the list
-    card *head = NULL;
-    card *tail = NULL;
+    struct card_s *head = NULL;
+    struct card_s *tail = NULL;
+    
+    // Dynamically allocate memory for head and tail
+    head = (struct card_s*)malloc(sizeof(struct card_s));
+    tail = (struct card_s*)malloc(sizeof(struct card_s));
     
     // Input file that may be used to read in the formatted output to populate linkedlist
     FILE *inp;
     inp = fopen("ordered_deck.txt", "r");
-    
-    // String used to hold line read from file
-    char line[LINE_SIZE];
-    
     if (inp == NULL) {
-        // File could not be opened. Print error and exit
         printf("ERROR: Could not open file. Ending Program\n");
         return -1;
     }
     
     
-    card temp_card;
-    char *temp;
-    int i;
+    
     while (fgets(line, LINE_SIZE, inp) != NULL) {
         
+        temp_card = pull_card_data(line);
         
+        add_to_end(head, tail, temp_card);
         
-        // Remove trailing new line when reading from file
-        line[strlen(line) - 1] = '\0';
-        
-        // Parse data and begin process of creating Linked List
-        if (line[0] == 'J') {
-            temp_card.value = 11;
-        } else if (line[0] == 'Q') {
-            temp_card.value = 12;
-        } else if (line[0] == 'K') {
-            temp_card.value = 13;
-        } else {
-            temp_card.value = atoi(&line[0]); // Pull integer value
-        }
-        temp = &line[2]; // Set pointer to begin pulling suit
-        i = 0;
-        
-        // Move pointer one more (covers the 10 case)
-        if (*temp == ' ') {
-            temp++;
-        }
-        // Loop until the end of the line, saving them into suit attribute
-        while (*temp != '\0') {
-            temp_card.suit[i] = *temp;
-            temp++;
-            i++;
-        }
-        
-        // Set to head if the head is currently NULL
-        if (head == NULL) {
-            head = &temp_card;
-            temp_card.prev = NULL;
-            temp_card.next = NULL;
-        } else {
-            // Add to end of list, setting next and prev pointers accordingly
-            tail->next = &temp_card;
-            temp_card.prev = tail;
-            temp_card.next = NULL;
-        }
-        
-        tail = &temp_card;
-        
-        printf("[%d : %s] -> ", temp_card.value, temp_card.suit);
-        
-        // Clear the suit string to avoid trailing characters
-        for (int j = 0; j < SUIT_LENGTH; j++) {
-            temp_card.suit[j] = '\0';
-        }
-        
+        printf("[%d : %s] -> ", temp_card->value, temp_card->suit);
+
+        temp_card = NULL; // Clear out the pointer to process next value
     }
     
     printf("\n===================================\n");
     
-    if (head != NULL) {
-        card *temp_ptr = head;
-        
-        printf("<%d:%s>", temp_ptr->value, temp_ptr->suit);
-        
-        while (temp_ptr != NULL) {
-            printf("[%d : %s] -> ", temp_ptr->value, temp_ptr->suit);
-            temp_ptr = temp_ptr->next;
-        }
-    } else {
-        printf("HEAD NULL");
-    }
+    
+    
+    // print_list(head);
     
     
     return 0;
+}
+
+struct card_s* pull_card_data(char line[]) {
+    
+    char *temp;
+    int i;
+    struct card_s *temp_card;
+    temp_card = (struct card_s*)malloc(sizeof(struct card_s));
+    
+    // Remove trailing new line when reading from file
+    line[strlen(line) - 1] = '\0';
+    
+    // Parse data and begin process of creating Linked List
+    if (line[0] == 'J') {
+        temp_card->value = 11;
+    } else if (line[0] == 'Q') {
+        temp_card->value = 12;
+    } else if (line[0] == 'K') {
+        temp_card->value = 13;
+    } else {
+        temp_card->value = atoi(&line[0]); // Pull integer value
+    }
+    temp = &line[2]; // Set pointer to begin pulling suit
+    i = 0;
+    
+    // Move pointer one more (covers the 10 case)
+    if (*temp == ' ') {
+        temp++;
+    }
+    // Loop until the end of the line, saving them into suit attribute
+    while (*temp != '\0') {
+        temp_card->suit[i] = *temp;
+        temp++;
+        i++;
+    }
+    
+    
+    
+    return temp_card;
+    
+}
+
+
+void print_list(struct card_s *card) {
+    
+    while (card != NULL) {
+        printf("[%d : %s] -> ", card->value, card->suit);
+        card = card->next;
+    }
+}
+
+void add_to_end(struct card_s *head, struct card_s *tail, struct card_s *temp_card) {
+    
+    if (head == NULL) {
+        // List is empty, add to the front
+        head = temp_card;
+        tail = temp_card;
+        temp_card->next = NULL;
+    } else {
+        // Add to end, whatever tail is pointing to
+        tail->next = temp_card;
+        temp_card->next = NULL;
+        tail = temp_card;
+    }
+    
 }
